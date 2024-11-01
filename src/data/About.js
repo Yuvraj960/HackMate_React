f7b42e0642d428f1701cbdef03cefaacafc50151
Yuvraj Gupta <yuvrajgupta956@gmail.com>
2024-11-01 23:32:30 +0530

    Added About Page

---
+++ src/data/About.js
@@ -0,0 +1,82 @@
+import developer from "../images/developer.png";
+import manager from "../images/manager.png";
+import designer from "../images/designer.png";
+import marketing from "../images/marketing.png";
+import service1 from "../images/service1.png";
+import service2 from "../images/service2.png";
+import service3 from "../images/service3.png";
+import service4 from "../images/service4.png";
+import service5 from "../images/service5.png";
+import service6 from "../images/service6.png";
+
+const teamMembers = [
+    {
+        name: "Yuvraj Gupta",
+        role: "Developer",
+        description:
+            "Developer with a passion for creating seamless user experiences.",
+        img: developer,
+    },
+    {
+        name: "Samridhi",
+        role: "Manager",
+        description:
+            "Manager dedicated to driving innovative tech projects forward.",
+        img: manager,
+    },
+    {
+        name: "Harry Garg",
+        role: "Designer",
+        description:
+            "Creative designer focused on merging aesthetics with functionality.",
+        img: designer,
+    },
+    {
+        name: "Riya Bansal",
+        role: "Marketing",
+        description:
+            "A strategic marketer focused on creating impactful narratives.",
+        img: marketing,
+    },
+];
+
+const services = [
+    {
+        title: "Upcoming Events & Workshops",
+        img: service1,
+        description:
+            "Discover nearby events and workshops to keep your skills sharp and stay informed.",
+    },
+    {
+        title: "Coding Contests & Challenges",
+        img: service2,
+        description:
+            "Find information on local coding contests and challenges to test your programming skills.",
+    },
+    {
+        title: "Hackathons & Innovation Sprints",
+        img: service3,
+        description:
+            "Get details on nearby hackathons and innovation sprints to join and collaborate with others.",
+    },
+    {
+        title: "Concerts & Tech Talks",
+        img: service4,
+        description:
+            "Explore local concerts and tech talks to enjoy live entertainment and gain industry insights.",
+    },
+    {
+        title: "Notes & Resources",
+        img: service5,
+        description:
+            "Access essential notes and references to support your participation hackathons.",
+    },
+    {
+        title: "Community & Networking Opportunities",
+        img: service6,
+        description:
+            "Stay connected tech communities and events to build your network.",
+    },
+];
+
+export { teamMembers, services };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,120 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from "react";
+import { teamMembers, services } from "../data/About";
+import "../styles/About.css";
+import logo from "../images/logo.png";
 
-const About = () => {
+const NewPage = () => {
+  const [selectedTeamMember, setSelectedTeamMember] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleTeamClick = (index) => {
+    setSelectedTeamMember(index === selectedTeamMember ? null : index);
+  };
+
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <div className="container-fluid about__heading">
+        <h1>ABOUT US</h1>
+      </div>
+      <div className="container about-container">
+        <section className="about-section">
+          <img src={logo} alt="HackMate Logo" className="about-logo" />
+          <div className="about-text">
+            <h3>Who is HackMate?</h3>
+            <p>
+              HackMate, your ultimate companion in the world of coding
+              competitions and innovation challenges! Our platform is dedicated
+              to bringing you a comprehensive directory of hackathons and tech
+              events from around the nation. Whether you&apos;re a seasoned
+              coder or a budding developer, HackMate is here to support your
+              journey with an extensive collection of practice materials. Dive
+              into our curated cheat sheets, explore educational YouTube links,
+              and leverage the power of AI tools to sharpen your skills. Join us
+              at HackMate, where preparation meets opportunity, and let&apos;s
+              hack the future together!
+            </p>
+          </div>
+        </section>
 
-export default About
+        <section className="team-section">
+          <h2>
+            <span>Come </span>meet us up close
+          </h2>
+          <p className="heading__info">
+            Every one of our team members is devoted to delivering the finest
+            possible outcomes.
+          </p>
+
+          <div className="team-cards">
+            {teamMembers.map((member, index) => (
+              <div
+                key={index}
+                className="team-card"
+                onClick={() => handleTeamClick(index)}
+              >
+                <img src={member.img} alt={member.name} className="team-img" />
+                <h3>{member.name}</h3>
+                {selectedTeamMember === index && (
+                  <p className="member-description">{member.description}</p>
+                )}
+              </div>
+            ))}
+          </div>
+
+          <button className="read-more" onClick={() => setShowPopup(true)}>
+            Read More
+          </button>
+
+          {showPopup && (
+            <div className="popup-overlay">
+              <div className="popup-content">
+                <button
+                  className="close-btn"
+                  onClick={() => setShowPopup(false)}
+                >
+                  &times;
+                </button>
+                <div className="popup-cards">
+                  {teamMembers.map((member, index) => (
+                    <div key={index} className="popup-card">
+                      <img src={member.img} alt={member.name} />
+                      <h3>{member.name}</h3>
+                      <p>{member.description}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
+        </section>
+
+        <section className="services-section">
+          <h2>
+            Our <span>Services</span>
+          </h2>
+          <p className="service__info">
+            Explore a range of resources and information designed to enhance
+            your tech experience and keep you connected with the local tech
+            scene.
+          </p>
+          <div className="service-cards">
+            {services.map((service, index) => (
+              <div key={index} className="service-card">
+                <img
+                  src={service.img}
+                  alt={service.title}
+                  className="service-img"
+                />
+                <h3>{service.title}</h3>
+                <p className="service-description">{service.description}</p>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default NewPage;
