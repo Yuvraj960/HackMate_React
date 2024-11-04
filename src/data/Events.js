066ba983f2fa30430890faedcd3a6367d17053e6
Yuvraj Gupta <yuvrajgupta956@gmail.com>
2024-11-04 23:38:46 +0530

    Added Events Page

---
+++ src/data/Events.js
@@ -0,0 +1,101 @@
+import event1 from '../images/event1.jpeg';
+import event2 from '../images/event2.jpeg';
+import event3 from '../images/event3.jpeg';
+import event4 from '../images/event4.jpg';
+import event5 from '../images/event5.jpeg';
+import event6 from '../images/event6.jpg';
+import event7 from '../images/event7.jpeg';
+import event8 from '../images/event8.jpeg';
+import event9 from '../images/event9.jpeg';
+import event10 from '../images/event10.jpg';
+import event11 from '../images/event11.jpeg';
+import event12 from '../images/event12.jpg';
+
+const eventsData = [
+    {
+        "id": 1,
+        "image": event1,
+        "date": " 23 August 2024",
+        "title": "Industrial 5G",
+        "description": "Join us on 23rd August 2024, at 4:00 PM for an online session with Dr. Prabhat Thakur on Industrial5G. Explore how 5G technology will revolutionize industries. Don’t miss this opportunity to learn about the future of industrial connectivity. "
+    },
+    {
+        "id": 3,
+        "image": event3,
+        "date": "25 August 2024",
+        "title": "Unleash GenAI",
+        "description": "We’re thrilled to announce a FREE Live Webinar on 'Getting Started with Gen AI' happening on August 25th, 2024, at 4 PM! 🎉. This exclusive session is brought to you by Coding Ninjas of Chitkara University in collaboration with UnsaidTalk"
+    },
+    {
+        "id": 4,
+        "image": event4,
+        "date": "13 March 2024",
+        "title": "Empower Her",
+        "description": "On this women's day let's have a healthy debate on topics related to women and what steps should be taken to empower women . Join us on 13th March at 2:00 PM , EIENSTEIN HALL , Galelio Block as Participant or as Audience "
+    },
+    {
+        "id": 5,
+        "image": event5,
+        "date": "13 August 2024",
+        "title": "Innovative With Generative AI",
+        "description": "Lets learn about innovative generative AI from out special guest and speaker Pranav Kumar on 13 August in Warren Buffet Hall , FLeming Block at 10:00 AM and learn and deep dive into world of AI there. Scan to register"
+    },
+    {
+        "id": 6,
+        "image": event6,
+        "date": "14Sept - 15 Sept 2024",
+        "title": "Hack The Mountains",
+        "description": "Hey Folks!We are pumped to announce our partnership with HTM 5.0, the ultimate hackathon experience! HTM Highlights Magic-● 17K+ hackers from 100+ countries● 185+ workshops with 150+ mentors● 1000+ projects and a whole lot more"
+    },
+    {
+        "id": 8,
+        "image": event8,
+        "date": "28 August 2024",
+        "title": "Coding Ninjas",
+        "description": "Join us on 28th August 2024 at 1:00 PM for Hack Your Brain 8.0: Design Thinking in Management Skills at Pierre Hall, Le Corbusier Block. Learn innovative problem-solving techniques from experts. "
+    },
+    {
+        "id": 10,
+        "image": event10,
+        "date": "13 December 2024",
+        "title": "GFG Hiring",
+        "description": "🌟 Calling all visionary minds and creative wizards! GEEKS FOR GEEKS, CUIET is on the lookout for exceptional executives to join our diverse and dynamic teams. Scan to Register for interview."
+    },
+    {
+        "id": 11,
+        "image": event11,
+        "date": "31 January 2024<",
+        "title": "A Young ENTERPRENUER's Guide",
+        "description": "IEEE CIET Student Branch is organizing an event 'From Idea to Impact: A Young Entrepreneur's Guide'.Embark on an entrepreneurial journey with Mr. Devanshu Kumar"
+    },
+    {
+        "id": 2,
+        "image": event2,
+        "date": "31 August 2024",
+        "title": "Sampoorna",
+        "description": "Good News for Freshers..!! Want to stand out in tech interviews Our Sampoorna Classroom Course covers all you need to know, starting August 31st Sampoorna Program Learn while you Earn This is the complete program for those who are starting their journe."
+    },
+    {
+        "id": 7,
+        "image": event7,
+        "date": "9 August 2024",
+        "title": "FRIDAY FUSION : Brew Your Code",
+        "description": " Get ready to do 2 Easy questions , 1 Medium coding question 5 Aptitide questions and 3 MCQS questions based on coding concepts and get chance to win excited prizes"
+    },
+    {
+        "id": 9,
+        "image": event9,
+        "date": "10 April 2024",
+        "title": "Tech Abhivyakti 2.0",
+        "description": "Get ready to experience a fusion of innovation and collaboration at the highly anticipated tech extravaganza - Tech अभिव्यक्ति 2.0: Innovation Unleashed (Final Round).10 April 2024 - 9-30 am onwards - Einstein Hall, Galileo Block . Meet our Judges .Excited to see you there .Win excited prizes"
+    },
+    {
+        "id": 12,
+        "image": event12,
+        "date": "29 March 2024",
+        "title": "FUSION FRIDAY",
+        "description": "🔈ATTENTION CODING ENTHUSIASTS ‼️ <br> Are you ready to unlock the full potential of your coding skills in an exhilarating,caffeine-fueled adventure? 💻. Get ready to win excited prizes."
+    },
+];
+
+export default eventsData;
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,10 +1,46 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import EventCard from "../components/EventCard";
+import eventsData from "../data/Events.js";
+import "../styles/Events.css";
 
-const Events = () => {
+const EventList = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    setEvents(eventsData);
+  }, []);
+
+  useEffect(() => {
+    const eventCount = document.querySelector(".eventCount");
+    const animateNumber = (start, end, duration) => {
+      let startTime = null;
+      const step = (timestamp) => {
+        if (!startTime) startTime = timestamp;
+        const progress = Math.min((timestamp - startTime) / duration, 1);
+        eventCount.textContent = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+          window.requestAnimationFrame(step);
+        }
+      };
+      window.requestAnimationFrame(step);
+    };
+    animateNumber(0, events.length, 800);
+  }, [events]);
+
   return (
-    <div>Events</div>
-  )
-}
+    <>
+      <div className="event__head">
+        <h1 className="eventCount"></h1>
+        <h1>Interesting Events For You</h1>
+      </div>
+      <div className="container event-container">
+        {events.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Events
+export default EventList;
