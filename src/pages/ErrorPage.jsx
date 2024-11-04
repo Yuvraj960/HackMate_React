eaad9431a589c7550cda8126279f2c6970e8200f
Yuvraj Gupta <yuvrajgupta956@gmail.com>
2024-11-05 00:02:37 +0530

    Added Error Page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,17 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className="error-page">
+      <h1>404</h1>
+      <h2>Sorry, the page you tried cannot be found</h2>
+      <Link to="/" className="btn">
+        Back Home
+      </Link>
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
